32c372bb7263df95866630756cd6df5d7264508d
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-02-09 16:05:07 +0000

    menu and category changin admin look amended

--- menu/admin.py
+++ menu/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import MenuItem, Category
 
 # Register your models here.
-admin.site.register(MenuItem)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+        'allergens',
+        'spicy',
+        'vegetarian',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(MenuItem, MenuAdmin)
 admin.site.register(Category)
