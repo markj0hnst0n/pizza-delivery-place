9f77e64d6f5fbd44aec9833ecaf2079e7e820bdb
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-02-09 16:50:06 +0000

    started menu app

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 # Create your views here.
+
+
+def menu(request):    
+    """ A view to show all menu items, including sorting and search queries """
+    menu = MenuItem.objects.all()
+    context = {
+        'Menu': menu,
+    }
+    return render(request, 'menu/menu.html', context)
