270d7d1c294d7697f1afac13f26f78ff6b1cd155
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-02-18 11:56:49 +0000

    wired up category links for menu

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import MenuItem
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import MenuItem, Category
 
 # Create your views here.
 
@@ -8,9 +10,27 @@ def menu(request):
     """ A view to show all menu items, including sorting and search queries """
 
     menu = MenuItem.objects.all()
-    
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            menu = menu.filter(queries)
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        menu = menu.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+
     context = {
         'menu': menu,
+        'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'menu/menu.html', context)
 
