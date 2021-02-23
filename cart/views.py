a4fa5b4b9a1edc08010917220f6994dc0206b26f
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-02-23 14:21:57 +0000

    update quantity button and remove button wired up but remove button not working

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
-from menu.models import MenuItem
 
 # Create your views here.
 
@@ -12,7 +11,6 @@ def cart(request):
 def add_to_cart(request, item_id):
     """ Add quantity of the menu item to the cart """
 
-    item = MenuItem.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     slot = request.session.get('slot', {})
@@ -28,6 +26,45 @@ def add_to_cart(request, item_id):
             cart[item_id] = quantity
 
         request.session['cart'] = cart
-        print(request.session['cart'])
 
         return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """ Adjust quantity of the menu item """
+
+    quantity = int(request.POST.get('quantity'))
+    slot = request.session.get('slot', {})
+    cart = request.session.get('cart', {})
+
+    if slot == {}:
+        messages.error(request, "You need to book a timeslot first!")
+        return redirect('timeslot')
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop[item_id]
+        
+        request.session['cart'] = cart 
+
+        return redirect(reverse('cart'))
+
+def remove_from_cart(request, item_id):
+    """ Remove menu item from bag """
+
+    slot = request.session.get('slot', {})
+    cart = request.session.get('cart', {})
+
+    try:
+        if slot == {}:
+            messages.error(request, "You need to book a timeslot first!")
+            return redirect('timeslot')
+        else:
+            cart.pop[item_id]
+            
+            request.session['cart'] = cart 
+
+            return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
