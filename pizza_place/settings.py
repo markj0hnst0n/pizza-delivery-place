19d5d663895f22a62e908e218069b6a4a8bdba51
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-02-26 12:25:21 +0000

    checkout template and view working correctly

--- pizza_place/settings.py
+++ pizza_place/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'timeslot',
     'cart',
     'checkout',
-    'crispy-forms',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
             'builtins': [
