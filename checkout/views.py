c2311127ce4dc7b27d68b61b1a885bad7cc8285d
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-02-26 14:48:02 +0000

    started adding stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -17,7 +17,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IDcERKBTecb2BhiyhQgAxXamUTTNBaO4scYLCIaZnHjbb9fYxJ472qqskVZiaJbp66qqxqr4rVbK4a0J88uGrd700X7IpDUEa',
+        'client_secret': 'test_secret'
     }
 
     return render(request, template, context)
