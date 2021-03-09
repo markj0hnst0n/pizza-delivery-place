87636561fb0fd1d89c926018c93a3dbfd7eaa6fc
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-03-09 14:33:54 +0000

    webhooks amended to add profile details to order

--- checkout/views.py
+++ checkout/views.py
@@ -118,7 +118,23 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.default_full_name,
+                    'email': profile.user.email,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'county': profile.default_county,
+                    'postcode': profile.default_postcode,
+                    'phone_number': profile.default_phone_number,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is not set')
--- pizza_place/settings.py
+++ pizza_place/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEVELOPMENT')
 
 ALLOWED_HOSTS = [
-    '127.0.0.1'
+    '127.0.0.1',
+    '4ff660456145.ngrok.io',
 ]
 
 
