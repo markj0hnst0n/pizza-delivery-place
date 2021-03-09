491e1d7b2d33701280dc8fe11f0a08e876308810
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-03-09 13:23:29 +0000

    order history added to profile page

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 
 from menu.models import MenuItem
 from timeslot.models import Timeslot
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from .models import Order, OrderLineItem
 from .forms import OrderForm
 from cart.contexts import cart_contents
@@ -135,16 +137,7 @@ def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-    # slot = request.session.get('slot', {})
-    # s_id = list(slot.keys())[list(slot.values()).index(True)]
-    # db_slot = get_object_or_404(Timeslot, pk=s_id)
 
-    # if db_slot.available_slots < 1:
-    #     if 'slot' in request.session:
-    #         messages.error(request, "All requested slots now booked.  Please try another slot")
-    #         del request.session['slot']
-    #         return redirect('timeslot')
-    # else:
     slot = request.session.get('slot', {})
     if 'slot' in request.session:
         s_id = list(slot.keys())[list(slot.values()).index(True)]
@@ -158,6 +151,27 @@ def checkout_success(request, order_number):
     
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_full_name': order.full_name,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county,
+                'default_postcode': order.postcode,
+                'default_phone_number': order.phone_number,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
