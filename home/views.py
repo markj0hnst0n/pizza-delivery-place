ec903230bdc0b5cdb2192fc94da9bc0c2a5dbfeb
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-03-11 17:10:24 +0000

    contact form being worked on

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.contrib import messages
 
-# Create your views here.
+from django.conf import settings
 
 
 def index(request):
@@ -13,4 +16,19 @@ def about(request):
 
 def contact(request):
     """ Shows and allows user to sent contact form """
+    if request.method == 'POST':
+        user_email = request.POST.get("email")
+        subject = render_to_string(
+            'home/contact_emails/contact_email_subject.txt')
+        body = render_to_string(
+            'home/contact_emails/contact_email_body.txt')
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            user_email
+        )
+        messages.success(request, "Contact From Sent")
+
     return render(request, 'home/contact.html')
