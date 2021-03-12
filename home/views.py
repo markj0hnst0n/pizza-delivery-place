dc4b35007d1ee7c3724b065d63f400ae8042313f
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-03-12 10:34:12 +0000

    contact function completed

--- home/views.py
+++ home/views.py
@@ -18,14 +18,37 @@ def contact(request):
     """ Shows and allows user to sent contact form """
     if request.method == 'POST':
         user_email = request.POST.get("email")
-        subject = render_to_string(
-            'home/contact_emails/contact_email_subject.txt')
-        body = render_to_string(
-            'home/contact_emails/contact_email_body.txt')
+        user_name = request.POST.get("name")
+        query = request.POST.get("query")
+        order_number = request.POST.get("order_number")
+
+        if order_number == "":
+            shop_subject = render_to_string(
+            'home/contact_emails/shop_contact_subject_no_order.txt')
+        else:
+            shop_subject = render_to_string(
+            'home/contact_emails/shop_contact_subject_order.txt',
+            {'order_number': order_number})
+        
+        shop_body = render_to_string(
+            'home/contact_emails/shop_contact_body.txt',
+            {'user_name': user_name, 'query': query})
+
+        customer_subject = render_to_string(
+            'home/contact_emails/contact_customer_confirmation_subject.txt')
+        customer_body = render_to_string(
+            'home/contact_emails/contact_customer_confirmation_body.txt')
         
         send_mail(
-            subject,
-            body,
+            shop_subject,
+            shop_body,
+            user_email,
+            [settings.DEFAULT_FROM_EMAIL]
+        )
+        
+        send_mail(
+            customer_subject,
+            customer_body,
             settings.DEFAULT_FROM_EMAIL,
             [user_email]
         )
