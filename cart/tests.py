aa74ae939dc1380af5b8abe46995c1f4b7839fa4
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-03-21 19:55:55 +0000

    more automated testing included for cart menu and profile apps

--- cart/tests.py
+++ cart/tests.py
@@ -1,1 +1,28 @@
 from django.test import TestCase
+from menu.models import MenuItem
+
+
+class TestCartViews(TestCase):
+
+    def test_cart_page_display(self):
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart/cart.html')
+
+    def test_add_to_cart(self):
+        test_item = MenuItem.objects.create(
+            name='Test Menu Item', price='1.00')
+        response = self.client.post(f'/add/{test_item}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_adjust_cart_item(self):
+        test_item = MenuItem.objects.create(
+            name='Test Menu Item', price='1.00')
+        response = self.client.post(f'/adjust/{test_item}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_remove_cart_item(self):
+        test_item = MenuItem.objects.create(
+            name='Test Menu Item', price='1.00')
+        response = self.client.post(f'/remove/{test_item}')
+        self.assertEqual(response.status_code, 200)
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestProfileViews(TestCase):
+
+    def test_admin_page_display(self):
+        response = self.client.get('profiles/admin/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_page_display(self):
+        response = self.client.get('profiles/')
+        self.assertEqual(response.status_code, 200)
