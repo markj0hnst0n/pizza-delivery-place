8e6a9015702cf4c04fd9c53a64929a1dd0f073d8
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-03-24 20:23:53 +0000

    further auto tests added

--- cart/tests.py
+++ cart/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.messages import get_messages
 from django.shortcuts import reverse
 from menu.models import MenuItem, Category
+from timeslot.models import Timeslot
 
 
 class TestCartViews(TestCase):
@@ -14,9 +15,14 @@ class TestCartViews(TestCase):
 
         new_product = MenuItem.objects.create(
             category=new_category,
-            name='test product',
+            name='test_product',
             price=1.00
             )
+        
+        new_timeslot = Timeslot.objects.create(
+            pk=1,
+            available_slots=1
+        )
 
     def test_view_cart(self):
         response = self.client.get(reverse('cart'), follow=True)
@@ -24,7 +30,7 @@ class TestCartViews(TestCase):
         self.assertTemplateUsed(response, 'cart/cart.html')
 
     def test_add_to_cart_with_no_timeslot(self):
-        new_product = MenuItem.objects.get(name='test product')
+        new_product = MenuItem.objects.get(name='test_product')
         post_data = {
             'quantity': '1',
             'redirect_url': '/'
@@ -40,9 +46,32 @@ class TestCartViews(TestCase):
         self.assertEqual(messages[0].tags, 'error')
         self.assertEqual(str(messages[0]), expected_message)
 
+    def test_add_to_cart_with_timeslot(self):
+        new_product = MenuItem.objects.get(name='test_product')
+        session = self.client.session
+        session['slot'] = {'1': True}
+        session.save()
+        post_data = {
+            'quantity': '1',
+            'redirect_url': '/'
+        }
+
+        response = self.client.post(reverse(
+                                'add_to_cart',
+                                kwargs={'item_id': new_product.id}),
+                                data=post_data)
+        messages = list(get_messages(response.wsgi_request))
+        expected_message = (f'Added {new_product.name} to cart!')
+
+        self.assertRedirects(response, '/')
+        self.assertEqual(messages[0].tags, 'success')
+        self.assertEqual(str(messages[0]), expected_message)
+
     def tearDown(self):
         new_category = Category.objects.get(name='test_category')
-        new_product = MenuItem.objects.get(product_name='test product')
+        new_product = MenuItem.objects.get(name='test_product')
+        new_timeslot = Timeslot.objects.get(pk=1)
 
         del new_category
         del new_product
+        del new_timeslot
