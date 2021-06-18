cc5457085cdafb6351ca24553f87f52190dcbc9f
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-06-18 10:01:23 +0100

    added min price validator on menu items.  menu price cannot be negative

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 
 class Category(models.Model):
@@ -33,7 +35,8 @@ class MenuItem(models.Model):
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, validators=[
+                                MinValueValidator(Decimal('0.01'))])
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     spicy = models.BooleanField(default=False)
